38883a6c0a65fa0f9e6c4c013b4de9836602eb4b
Ashley Cruz <ashleynayellicruz@gmail.com>
2022-02-04 00:11:49 -0600

    app structure and redux configuration

---
+++ src/reducers/categoryReducer.js
@@ -0,0 +1,13 @@
+
+const initialState = {
+    categories: [],
+    activeCategory: []
+}
+
+export const categoryReducer = (state = initialState, action) => {
+    switch (action.type) {
+    
+        default:
+            return state;
+    }
+};
